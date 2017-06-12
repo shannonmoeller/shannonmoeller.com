eda0bbed8cbc6dcc228a820014ae8cde14d91264
Shannon Moeller <me@shannonmoeller.com>
2017-06-12 01:16:52 -0400

    Semantic text formatting.

--- tasks/statics.js
+++ tasks/statics.js
@@ -1,14 +1,28 @@
 import gulp from 'gulp';
+import svgstore from 'gulp-svgstore';
 
-export async function statics() {
+export async function staticsAssets() {
 	return gulp
-		.src('./src/clien*/{assets/**/*.*,**/*.html}')
+		.src('./src/clien*/{assets/media/**/*.*,**/*.html}')
 		.pipe(gulp.dest('dist'));
 }
 
-gulp.task('statics', () => {
+export async function staticsIcons() {
+	return gulp
+		.src('./src/client/assets/icons/*.svg')
+		.pipe(svgstore())
+		.pipe(gulp.dest('dist/client/assets/media'));
+}
+
+export const statics = gulp.parallel(
+	staticsAssets,
+	staticsIcons,
+);
+
+gulp.task('statics', async () => {
 	if (process.env.NODE_ENV === 'development') {
-		gulp.watch('./src/clien*/{assets/**/*.*,**/*.html}', statics);
+		gulp.watch('./src/clien*/{assets/media/**/*.*,**/*.html}', staticsAssets);
+		gulp.watch('./src/client/assets/icons/*.svg', staticsIcons);
 	}
 
 	return statics();
