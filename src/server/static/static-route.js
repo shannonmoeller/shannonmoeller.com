2c012e590f801805425c51d74db972011d9d4d87
Shannon Moeller <me@shannonmoeller.com>
2017-06-13 14:44:24 -0400

    Moved static options out of route.

--- src/server/static/static-route.js
+++ src/server/static/static-route.js
@@ -1,10 +1,14 @@
 import { Router, static as serve } from 'express';
 import { resolve } from 'path';
 
+const options = {
+	maxAge: process.env.NODE_ENV === 'development'
+		? 0
+		: '1d',
+};
+
 export function getStatic(path) {
-	return serve(resolve(__dirname, path), {
-		maxAge: process.env.NODE_ENV === 'development' ? 0 : '1d',
-	});
+	return serve(resolve(__dirname, path), options);
 }
 
 export function routeStatic() {
