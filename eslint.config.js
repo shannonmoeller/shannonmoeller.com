a7663030e395fe097237c60ace65a21fb22bbd2a
Shannon Moeller <me@shannonmoeller.com>
2024-11-17 16:34:05 -0700

    resume layout updates, job highlights

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,13 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-	js.configs.recommended,
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-		},
-	},
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
 ];
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,5 @@
 export default {
-	printWidth: 120,
-	singleQuote: true,
-	tabWidth: 2,
-	useTabs: true,
+  printWidth: 120,
+  singleQuote: true,
+  tabWidth: 2,
 };
