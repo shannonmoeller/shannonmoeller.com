cc45a984d2f21441f57fac649d9bdabbf7ea1bb0
Shannon Moeller <me@shannonmoeller.com>
2024-11-18 10:33:39 -0700

    remove old code

--- src/client/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import './shim/feature-detection';
-import './shim/native-custom-elements';
-import '@webcomponents/custom-elements';
-import 'dom4';
-
-import { MoePerspectiveElement } from '../moe-perspective/moe-perspective-element';
-
-customElements.define('moe-perspective', MoePerspectiveElement);
